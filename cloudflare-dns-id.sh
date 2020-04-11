9e9ceeff135fa294761e3901c187e7cd6c6e25b6
dproot <63499273+dproot@users.noreply.github.com>
2020-04-11 20:22:28 +0800

    Update cloudflare-dns-id.sh

--- cloudflare-dns-id.sh
+++ cloudflare-dns-id.sh
@@ -6,10 +6,7 @@
 #Create the CNAME alias (example.com --> dynamic.example.com 1)
 #Fetch The A-record ID:
 
-curl -X GET "https://api.cloudflare.com/client/v4/zones/**Zone ID**/dns_records?type=A&name=dynamic" \
-     -H "Host: api.cloudflare.com" \
-     -H "User-Agent: ddclient/3.9.0" \
-     -H "Connection: close" \
+curl -X GET "https://api.cloudflare.com/client/v4/zones/**Zone ID**/dns_records?type=A" \
      -H "X-Auth-Email: example@example.com" \
      -H "X-Auth-Key: "**Authorization key**" \
      -H "Content-Type: application/json"
